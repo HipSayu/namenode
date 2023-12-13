aa3e78782e96bd69a09c692086fa3d09919a7c59
HipSayu <phamngochiepgamer@gmail.com>
2023-12-13 21:03:09 +0700

    namenode

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,3 @@
-import nameNode from '../models/nameNode';
-import axios from 'axios';
 import fs from 'fs'
 import path from 'path';
 import metaData from '../models/metadata';
@@ -12,65 +10,13 @@ const handleHelloWord = (req, res) => {
 
 
 
-const Requireheartbeat = () => {
-    const heartbeatEndPoint = 'http://localhost:3001/namenode/hearbeat'
-    setInterval(async () => {
-       await axios.post(heartbeatEndPoint, {
-            check: 'heartBeat'
-        })
-        .then(async (res)=>{
-            // có heartBeat thì lưu vào còn sống
-            var heartBeat = res.data
-            console.log('datheartBeataNode1', heartBeat)
-            await ManagerDataNode.find({NameNodeID :"HipdzVao"}).then(async (data, err) => {
-                data = data[0];
-                let dataNode1 = data.dataNode1
-                console.log('dataNode1', dataNode1)
-                console.log('Alive', dataNode1.Alive)
-                let Alive = dataNode1.Alive
-                if (Alive != 'Yes') {
-                    dataNode1.Alive='Yes'
-                    await ManagerDataNode.updateOne(
-                        {
-                            "NameNodeID": 'HipdzVao'
-                        },
-                        {
-                            $set : {dataNode1 : dataNode1}
-                        }
-                    )
-                }
-                
-            })
-        })
-        .catch(async (err)=>{
-                 // có heartBeat thì lưu vào dead
-                console.log( 'check error')
-                await ManagerDataNode.find({NameNodeID :"HipdzVao"}).then(async (data, err) => {
-                    data = data[0];
-                    let dataNode1 = data.dataNode1
-                    console.log('dataNode1', dataNode1)
-                    console.log('Alive', dataNode1.Alive)
-                    let Alive = dataNode1.Alive
-                    if (Alive != 'No') {
-                        dataNode1.Alive='No'
-                        await ManagerDataNode.updateOne(
-                            {
-                                "NameNodeID": 'HipdzVao'
-                            },
-                            {
-                                $set : {dataNode1 : dataNode1}
-                            }
-                        )
-                    }
-                })
-        })
-        console.log('Sent HeartBeat');
-    }, 4000);
-};
 
-
-
-
+function getRandomUniqueElement(arr) {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    var randomElement = arr[randomIndex];
+    arr.splice(randomIndex, 1);
+    return randomElement;
+}
 
 const hanldeCheckRequest = async (req, res) => {
     const Replication = 2;
@@ -111,14 +57,26 @@ const hanldeCheckRequest = async (req, res) => {
             }
         //    console.log('DataNode', DataNodeAlive)
            //random
-
-            for (let i=0; i<NumberChunk;i++ ){
-                var randomIndex = Math.floor(Math.random() * DataNodeAlive.length);
-                var randomIndex1 = Math.floor(Math.random() * DataNodeAlive.length);
-                var randomIndex2 = Math.floor(Math.random() * DataNodeAlive.length);
-                DatanodeWrite.push(DataNodeAlive[randomIndex])
-                DatanodeReplication1.push(DataNodeAlive[randomIndex1])
-                DatanodeReplication2.push(DataNodeAlive[randomIndex2])
+            if(DataNodeAlive.length >=3){
+                for (let i=0; i< NumberChunk;i++ ){
+                    let DataNodeAlive_Copy = DataNodeAlive.slice()
+                    DatanodeWrite.push(getRandomUniqueElement(DataNodeAlive_Copy));
+                    DatanodeReplication1.push(getRandomUniqueElement(DataNodeAlive_Copy));
+                    DatanodeReplication2.push(getRandomUniqueElement(DataNodeAlive_Copy));
+                }
+            }else if (DataNodeAlive.length == 2){
+                for (let i=0; i< NumberChunk;i++ ){
+                    var random = Math.floor(Math.random() * DataNodeAlive.length)
+                    DatanodeWrite.push(DataNodeAlive[0]);
+                    DatanodeReplication1.push(DataNodeAlive[1]);
+                    DatanodeReplication2.push(DataNodeAlive[random]);
+                }
+            }else{
+                for (let i=0; i< NumberChunk;i++ ){
+                    DatanodeWrite.push((DataNodeAlive[0]));
+                    DatanodeReplication1.push(DataNodeAlive[0]);
+                    DatanodeReplication2.push(DataNodeAlive[0]);
+                }
             }
             // console.log('DatanodeWrite', DatanodeWrite)
             // console.log('DatanodeReplication1', DatanodeReplication1)
@@ -132,7 +90,7 @@ const hanldeCheckRequest = async (req, res) => {
                     indexFile: i,
                     DataNode:DatanodeWrite[i-1].address,
                     DatanodeReplication1:DatanodeReplication1[i-1].address,
-                    DatanodeReplication2:DatanodeReplication1[i-1].address,
+                    DatanodeReplication2:DatanodeReplication2[i-1].address,
                 });
             }
             console.log('check Meta', metaDatas);
@@ -146,51 +104,7 @@ const hanldeCheckRequest = async (req, res) => {
             })
         });
     }
-    // if (Request == 'Write') {
-    //     var file = Clients.file
-    //     await nameNode.find({}).then(async (data, err) => {
-    //         var metaDatas = [];
-    //         data = data[0];
-    //         var dataNodeSize= data.DataNodeSize;
-    //         var dataNode = data.DataNode;
-    //         // lấy random các dataNode còn trống
-
-    //         const keys = Object.keys(dataNode);
-    //         console.log(keys)
-           
-    //         var shuffledArray = keys.slice(); 
-    //         var DataNodeInser = [];
-        
-    //         for (var i = 0; i < NumberChunk; i++) {
-    //             var randomIndex = Math.floor(Math.random() * (shuffledArray.length - i));
-    //             var selectedElement = shuffledArray[randomIndex];
-    //             DataNodeInser.push(selectedElement);
-    //             shuffledArray[randomIndex] = shuffledArray[shuffledArray.length - 1 - i];
-    //         }
-    //         console.log(DataNodeInser)
-
-
-    //         for (let i = 0; i < NumberChunk; i += 1) {
-    //             metaDatas.push({
-    //                 nameFile: Clients.name,
-    //                 desc: Clients.description,
-    //                 indexFile: i,
-    //                 DataNode: dataNode[`${DataNodeInser[i]}`],
-    //             });
-    //         }
-    //         console.log('check Meta', metaDatas);
-    //         await metaData
-    //                     .insertMany(metaDatas)
-    //                     .then(console.log('Write Succesfully'));
-
-    //         res.send({
-    //                 AddressDataNode: metaDatas,
-    //                 file: file,
-    //                 request : 'write'
-    //         })
-            
-    //     });
-    // }
+    
     else {
        
         const name = Clients.name
@@ -256,5 +170,4 @@ const hanldeCheckRequest = async (req, res) => {
 module.exports = {
     handleHelloWord,
     hanldeCheckRequest,
-    Requireheartbeat
 };
